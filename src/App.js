49122d74447e6bf0c3c8de9d9a9bedc35dafd08a
AdamHjerpe <adam.hjerpe@gmail.com>
2019-02-09 11:44:08 +0100

    Basic signup and auth working, needs refining

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
 import React, { Component } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom'
+
+import AuthPage from './pages/Auth'
+import EventsPage from './pages/Events'
+import BookingsPage from './pages/Bookings'
+import MainNavigation from './components/Navigation/MainNavigation'
+
+import './App.scss'
 
 class App extends Component {
   render () {
     return (
-      <div className='App'>
-        <header className='App-header'>
->
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className='App-link'
-            href='https://reactjs.org'
-            target='_blank'
-            rel='noopener noreferrer'
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <BrowserRouter>
+        <React.Fragment>
+          <MainNavigation />
+          <main className='main-content'>
+            <Switch>
+              <Redirect from='/' to='auth' exact />
+              <Route path='/auth' component={AuthPage} />
+              <Route path='/events' component={EventsPage} />
+              <Route path='/bookings' component={BookingsPage} />
+            </Switch>
+          </main>
+        </React.Fragment>
+      </BrowserRouter>
     )
   }
 }
---
+++ src/pages/Bookings.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+import './Bookings.scss'
+
+class BookingsPage extends Component {
+  render () {
+    return (<h1>The Bookings Page</h1>)
+  }
+}
+
+export default BookingsPage
